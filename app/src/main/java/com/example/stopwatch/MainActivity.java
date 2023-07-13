b063160af60a45c8ccd3f77d6533c4219f87d52c
Abhishek Gupta <abhishekgupta3247@gmail.com>
2023-07-13 19:25:29 +0530

    applied OnClickListener on MainActivity.java

--- app/src/main/java/com/example/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.stopwatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView timerTv;
+    Button buttonStart, buttonStop, buttonReset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        timerTv = findViewById(R.id.timerText);
+        buttonStart = findViewById(R.id.btnStart);
+        buttonStop = findViewById(R.id.btnStop);
+        buttonReset = findViewById(R.id.btnReset);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
